d1fff6a9ae9b4105e407ee240bfbe50a6dda2c04
batfaldu59 <baptistefalbierski7@gmail.com>
2020-12-07 23:04:09 +0100

    First view Produicts

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Sodium\compare;
 
 class ArticlesController extends AbstractController
 {
@@ -16,5 +18,13 @@ class ArticlesController extends AbstractController
         return $this->render('articles/index.html.twig', [
             'controller_name' => 'ArticlesController',
         ]);
+    }
+
+    /**
+     * @Route("/nosproduits", name="app_produits")
+     */
+    public function afficheProduit(ProduitRepository $produitRepository) {
+        $produits = $produitRepository->findBy([], ["id" => "DESC"]);
+        return $this->render("articles/affichArticle.html.twig", compact("produits"));
     }
 }
