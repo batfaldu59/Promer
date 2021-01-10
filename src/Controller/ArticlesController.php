d4433cd3b1f0b61fbaa8c7680a228d8aeb4ebe41
batfaldu59 <baptistefalbierski7@gmail.com>
2021-01-10 20:11:44 +0100

    Filter for product

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Classe\Search;
 use App\Entity\Produit;
+use App\Form\SearchType;
 use App\Repository\ProduitRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Sodium\compare;
@@ -22,10 +25,18 @@ class ArticlesController extends AbstractController
     /**
      * @Route("/nosproduits", name="app_produits")
      */
-    public function afficheProduit() {
+    public function afficheProduit(Request $req) {
         $produits = $this->entityManager->getRepository(Produit::class)->findBy([], ["id" => "DESC"]);
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
+        $form->handleRequest($req);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $produits = $this->entityManager->getRepository(Produit::class)->findWithSearch($search);
+
+        }
         return $this->render("articles/index.html.twig", [
-            'products' => $produits
+            'products' => $produits,
+            'form' => $form->createView()
         ]);
     }
 
